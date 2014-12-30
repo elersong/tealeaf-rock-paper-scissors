2d90f9a866ed654095f0af63b247a617581ee1da
Grey Elerson <grey.l.elerson@gmail.com>
2014-12-30 21:01:03 +0000

    explain why user wins or loses in prompt

--- rps.rb
+++ rps.rb
@@ -79,11 +79,11 @@ def winner(user, comp)
       when comp
         "It's a TIE!!!"
       when "P"
-        comp == "R" ? "User Wins!" : "Computer Wins!"
+        comp == "R" ? "Paper covers rock! User Wins!" : "Scissors cut paper! Computer Wins!"
       when "R"
-        comp == "S" ? "User Wins!" : "Computer Wins!"
+        comp == "S" ? "Rock breaks scissors! User Wins!" : "Paper covers Rock! Computer Wins!"
       when "S"
-        comp == "P" ? "User Wins!" : "Computer Wins!"
+        comp == "P" ? "Scissors cut paper! User Wins!" : "Rock breaks scissors! Computer Wins!"
     end
   puts string
 end
